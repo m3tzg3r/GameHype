93a6fcd32336811e76bc7c7ebb03d7aafd32ce19
m3tzg3r <34689231+m3tzg3r@users.noreply.github.com>
2023-11-12 11:18:10 -0900

    Update nuggetshype.py
    Added poweron script as this did not work if the lights were not already on.  Also needed to add a sleep after poweron otherwise the auth and colorcode scripts didnt work. 
    
    Reworded some comments.

--- nuggetshype.py
+++ nuggetshype.py
@@ -12,6 +12,16 @@ from datetime import datetime
 log_file_name = 'denverhype.log'
 logging.basicConfig(filename=log_file_name, level=logging.INFO,
                     format='%(asctime)s [%(levelname)s]: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+def send_poweron():
+    message = {"msg":{"cmd":"turn","data":{"value":1}}}
+    group = "239.255.255.250"
+    port = 4001
+    ttl = 2
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+    json_result = json.dumps(message)
+    logging.info(f"Sending: {json_result}")
+    sock.sendto(bytes(json_result, "utf-8"), (group, port))
 
 def send_razer_command(pt):
     message = {"msg": {"cmd": "razer", "data": {"pt": pt}}}
@@ -24,7 +34,7 @@ def send_razer_command(pt):
     logging.info(f"Sending: {json_result}")
     sock.sendto(bytes(json_result, "utf-8"), (group, port))
 
-def send_message():
+def send_keepalive():
     message = {"msg": {"cmd": "status", "data": {}}}
     group = "239.255.255.250"
     port = 4001
@@ -52,20 +62,24 @@ if response.status_code == 200:
             if "DEN" in game.get("gameCode", ""):
                 game_code = game.get("gameCode", "N/A")
                 logging.info(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} [INFO]: The Denver Nuggets are playing today. Game Code: {game_code}")
-
-                # Execute the first script
+                
+                # Execute the poweron script and wait 1 second before continuing 
+                send_poweron()
+                time.sleep(1)
+                
+              # Execute the auth script
                 send_razer_command("uwABsQEK")
 
-                # Execute the second script
+                # Execute the color code script
                 send_razer_command("uwAgsAAKAAD///8AAAD///8AAAD/AAD///8AAAD///8AAAD/IQ==")
 
-                # Run the third script in a loop for the specified duration
+                # Run the keep alive script in a loop for the specified duration
                 total_duration = 4 * 60 * 60  # 4 hours
                 interval = 30  # 30 seconds
                 num_iterations = total_duration // interval
 
                 for _ in range(num_iterations):
-                    send_message()
+                    send_keepalive()
                     time.sleep(interval)
 
         logging.info(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} [INFO]: Script executed successfully.")

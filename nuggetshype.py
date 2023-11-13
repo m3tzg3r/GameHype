ed6ade10f8963885bdfa1455f169f562f6cafe1c
m3tzg3r <34689231+m3tzg3r@users.noreply.github.com>
2023-11-13 08:01:52 -0900

    Update nuggetshype.py
    added logic to verify that API data is current.. Seems like endpoint can have some delay in updating and could give false positives. 
    
    minor formatting fixes

--- nuggetshype.py
+++ nuggetshype.py
@@ -9,9 +9,9 @@ import logging
 from datetime import datetime
 
 # Configure logging
-log_file_name = 'denverhype.log'
-logging.basicConfig(filename=log_file_name, level=logging.INFO,
-                    format='%(asctime)s [%(levelname)s]: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+log_file_name = 'NuggetsHype.log'
+logging.basicConfig(filename=log_file_name, level=logging.INFO, format='%(asctime)s [%(levelname)s]: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
 def send_poweron():
     message = {"msg":{"cmd":"turn","data":{"value":1}}}
     group = "239.255.255.250"
@@ -56,34 +56,39 @@ if response.status_code == 200:
     # Parse the JSON data
     data = response.json()
 
-    # Check if "DEN" is in the output and indicate if the Denver Nuggets are playing
-    if "DEN" in str(data):
-        for game in data["scoreboard"]["games"]:
-            if "DEN" in game.get("gameCode", ""):
-                game_code = game.get("gameCode", "N/A")
-                logging.info(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} [INFO]: The Denver Nuggets are playing today. Game Code: {game_code}")
-                
-                # Execute the poweron script and wait 1 second before continuing 
-                send_poweron()
-                time.sleep(1)
-                
-              # Execute the auth script
-                send_razer_command("uwABsQEK")
+    # Check if API is current and gameDate matches current date.
+    current_date = datetime.now().strftime('%Y-%m-%d')
+    if data["scoreboard"]["gameDate"] == current_date:
+        # Check if "DEN" is present in the output and indicate if the Denver Nuggets are playing
+        if any("DEN" in game.get("gameCode", "") for game in data["scoreboard"]["games"]):
+            for game in data["scoreboard"]["games"]:
+                if "DEN" in game.get("gameCode", ""):
+                    game_code = game.get("gameCode", "N/A")
+                    logging.info(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} [INFO]: The Denver Nuggets are playing today. Game Code: {game_code}")
+                    
+                    # Execute power on and sleep 5 seconds
+                    send_poweron()
+                    time.sleep(1)
 
-                # Execute the color code script
-                send_razer_command("uwAgsAAKAAD///8AAAD///8AAAD/AAD///8AAAD///8AAAD/IQ==")
+                    # Execute the first script
+                    send_razer_command("uwABsQEK")
 
-                # Run the keep alive script in a loop for the specified duration
-                total_duration = 4 * 60 * 60  # 4 hours
-                interval = 30  # 30 seconds
-                num_iterations = total_duration // interval
+                    # Execute the second script
+                    send_razer_command("uwAgsAAKAAD///8AAAD///8AAAD/AAD///8AAAD///8AAAD/IQ==")
 
-                for _ in range(num_iterations):
-                    send_keepalive()
-                    time.sleep(interval)
+                    # Run the third script in a loop for the specified duration
+                    total_duration = 4 * 60 * 60  # 4 hours
+                    interval = 30  # 30 seconds
+                    num_iterations = total_duration // interval
 
-        logging.info(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} [INFO]: Script executed successfully.")
+                    for _ in range(num_iterations):
+                        send_message()
+                        time.sleep(interval)
+
+            logging.info(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} [INFO]: Script executed successfully.")
+        else:
+            logging.info(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} [INFO]: Denver Nuggets are not playing today.")
     else:
-        logging.info(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} [INFO]: Denver Nuggets are not playing today.")
+        logging.info(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} [INFO]: API not updated yet. Current date: {current_date}. Game date retrieved from API: {data['scoreboard']['gameDate']} :(")
 else:
     logging.error(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} [ERROR]: Failed to retrieve data from the endpoint.")

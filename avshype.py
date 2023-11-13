84d2352361bd4ea3451f8b6f65f552eaf5486203
m3tzg3r <34689231+m3tzg3r@users.noreply.github.com>
2023-11-12 19:06:42 -0900

    Update avshype.py
    Fixed to use only current date games.. Before it was using the whole week.
    
    added send_poweron as razer hack will not work if lights are not on first.

--- avshype.py
+++ avshype.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import requests
 import json
 import socket
@@ -10,6 +12,17 @@ from datetime import datetime
 log_file_name = 'avshype.log'
 logging.basicConfig(filename=log_file_name, level=logging.INFO,
                     format='%(asctime)s [%(levelname)s]: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+def send_poweron():
+    message = {"msg":{"cmd":"turn","data":{"value":1}}}
+    group = "239.255.255.250"
+    port = 4001
+    ttl = 2
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+    json_result = json.dumps(message)
+    logging.info(f"Sending: {json_result}")
+    sock.sendto(bytes(json_result, "utf-8"), (group, port))
 
 def send_razer_command(pt):
     message = {"msg": {"cmd": "razer", "data": {"pt": pt}}}
@@ -43,11 +56,15 @@ def is_colorado_avalanche_playing():
         schedule_data = response.json()
 
         for game_day in schedule_data.get("gameWeek", []):
-            for game in game_day.get("games", []):
-                home_team_id = game.get("homeTeam", {}).get("id")
-                away_team_id = game.get("awayTeam", {}).get("id")
+            game_day_date = game_day.get("date")
 
-                if home_team_id == 21 or away_team_id == 21:
+            # Check if the game date matches the current date
+            if game_day_date == current_date:
+                for game in game_day.get("games", []):
+                    home_team_id = game.get("homeTeam", {}).get("id")
+                    away_team_id = game.get("awayTeam", {}).get("id")
+
+                if home_team_id == 21 or away_team_id == 21: # This is the Colorado Avalanche team code and will be different for other teams.
                     return True
 
         return False
@@ -58,15 +75,18 @@ def is_colorado_avalanche_playing():
 if is_colorado_avalanche_playing():
     logging.info(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} [INFO]: The Colorado Avalanche are playing today.")
 
-    # Execute the first script
+    # Execute the poweron script and wait 1 second before continuing 
+    send_poweron()
+    time.sleep(1)
+                
+    # Execute the auth script
     send_razer_command("uwABsQEK")
 
-    # Execute the second script
-
+    # Execute the color code script
     send_razer_command("uwAgsAAKAAD//wAAAAD//wAAAAD/AAD//wAAAAD//wAAAAD/IQ==")
 
-    # Run the third script in a loop for the specified duration
-    total_duration = 8 * 60 * 60  # 8 hours
+    # Run the keep alive script in a loop for the specified duration
+    total_duration = 4 * 60 * 60  # 4 hours
     interval = 30  # 30 seconds
     num_iterations = total_duration // interval
 
